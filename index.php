8cf58e45a33c0097cd5758f0a28c34ff7b1a8393
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-01-09 15:23:07 +0100

    feat: filtro per il parcheggio

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
+];
+
+// valori dal get
+$parking = $_GET['parking'] ?? false;
+$vote = $_GET['vote'] ?? false;
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -51,33 +56,71 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Fine Bootstrap -->
 </head>
+
 <body>
-    <h1 class="text-center my-3">PHP Hotel</h1>
-    <!-- Table -->
-    <table class="table table-striped mt-5 w-75 m-auto border">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Parking</th>
-                <th>Vote</th>
-                <th>Distance to center</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach($hotels as $hotel){
-                    echo '<tr>';
-                    foreach($hotel as $key => $info){
-                        if($info === true)$info = 'true';
-                        if($info === false)$info = 'false';
-                        echo "<td>$info</td>";
+    <main class="w-75 m-auto">
+        <h1 class="text-center my-3">PHP Hotel</h1>
+        <form action="index.php" method="GET" class="mt-5 mb-2 row align-items-center justify-content-end">
+            <div class="col-auto">
+                <label class="visually-hidden" for="autoSizingSelect">Preference</label>
+                <select class="form-select" id="autoSizingSelect" name="vote">
+                    <option selected>Voto</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <div class="col-auto">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="autoSizingCheck" name="parking">
+                    <label class="form-check-label" for="autoSizingCheck">
+                        Parcheggio
+                    </label>
+                </div>
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary">Cerca</button>
+            </div>
+        </form>
+        <!-- Table -->
+        <table class="table table-striped m-auto border">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrizione</th>
+                    <th>Parcheggio</th>
+                    <th>Voto</th>
+                    <th>Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // recupera tutti gli hotel
+                foreach ($hotels as $hotel) {
+                    $element = '<tr>';
+                    $save = true;
+                    // recupera le informazini
+                    foreach ($hotel as $key => $info) {
+                        // se il filtro è su parking e la chiave è parking
+                        if($parking === 'on' && $key === 'parking'){
+                            // se è falsa imposta la variabile su false
+                            if($info === false)$save = false;
+                        }
+                        if ($info === true) $info = 'true';
+                        if ($info === false) $info = 'false';
+                        $element .= "<td>$info</td>";
                     }
-                    echo '</tr>';
+                    $element .= '</tr>';
+                    // se la variabile è diversa da true non mostra i valori
+                    if($save === true)echo $element;
                 }
-            ?>
-        </tbody>
-    </table>
-    <!-- Fine Table -->
+                ?>
+            </tbody>
+        </table>
+        <!-- Fine Table -->
+    </main>
 </body>
+
 </html>
